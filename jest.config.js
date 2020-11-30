b7fc9201a3005f85fafce8da1709c569a23372f6
Foundy <foundy@foundy.io>
2020-11-30 15:49:43 +0900

    feat: generate red application

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/blue', '<rootDir>/apps/green'],
+  projects: [
+    '<rootDir>/apps/blue',
+    '<rootDir>/apps/green',
+    '<rootDir>/apps/red',
+  ],
 };
